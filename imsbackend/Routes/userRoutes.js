3ada191425db501a1cce79771993524c137b9c81
Kalayu Redae <kalayuredae2@gmail.com>
2026-01-01 22:13:34 +0300

    update user Registration

--- imsbackend/Routes/userRoutes.js
+++ imsbackend/Routes/userRoutes.js
@@ -14,19 +14,19 @@ app.use(function (req, res, next) {
   next();
 });
 
-// const attachments = createMulterMiddleware(
-//   'uploads/profileImages', // Destination folder
-//   'profileImage', // Prefix for filenames
-//   ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'application/pdf', 'application/msword'] // Allowed types
-// );
+const attachments = createMulterMiddleware(
+  'uploads/profileImages', // Destination folder
+  'profileImage', // Prefix for filenames
+  ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'application/pdf', 'application/msword'] // Allowed types
+);
 
-// exports.uploadFilesMiddleware = attachments.fields([
-//   { name: 'profileImage', maxCount: 1 },// Single file for profileImage
-//   { name: 'images', maxCount: 10 }, // upto to 10 images
-//   { name: 'documents', maxCount: 10 }, // Up to 10 files for documents
-// ]);
+exports.uploadFilesMiddleware = attachments.fields([
+  { name: 'profileImage', maxCount: 1 },// Single file for profileImage
+  { name: 'images', maxCount: 10 }, // upto to 10 images
+  { name: 'documents', maxCount: 10 }, // Up to 10 files for documents
+]);
 
-//router.post("/signup",this.uploadFilesMiddleware,authoController.signup)
+router.post("/signup",this.uploadFilesMiddleware,authoController.signup)
 router.post("/login",authoController.login)
 
 router.post('/forgetPassword', authoController.forgetPassword);
