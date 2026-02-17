24928b4856e037b1db52b7b7a496ff712f3a1f40
ashenafiadane <ashenafiadane11@gmail.com>
2026-02-17 10:18:06 +0300

    navbarchanged

--- ims-frontend/src/components/Navbar.vue
+++ ims-frontend/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="bg-white shadow p-4 flex justify-between items-center">
-    <h1 class="text-xl font-bold text-primary">IMS Dashboard</h1>
+    <h1 class="text-xl font-bold text-primary">Dashboard</h1>
     <div>
       <button class="px-4 py-2 bg-primary text-white rounded hover:bg-primary-dark transition">Logout</button>
     </div>
